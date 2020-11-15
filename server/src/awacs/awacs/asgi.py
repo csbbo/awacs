a9c12b03ae1f6182583fd18fecc41b546a9d7091
shaobo.chen <shaobo.chen@chaitin.com>
2020-11-15 16:01:45 +0800

    feat: add asgi and websocket test

--- server/src/awacs/awacs/asgi.py
+++ server/src/awacs/awacs/asgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from awacs.websocket import websocket_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awacs.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
