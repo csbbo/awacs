50f369d405807934b7a84375356f6c8d5ad48bfe
shaobo.chen <shaobo.chen@chaitin.com>
2020-11-24 10:28:06 +0800

    fix: run.sh add sleep before migrate

--- server/src/awacs/run.sh
+++ server/src/awacs/run.sh
@@ -3,10 +3,10 @@
 echo -e "\e[1;31m 1. init db \e[0m"
 while :; do
     # Wait PostgreSQL
+    sleep 3
     if python3 manage.py migrate; then
         break
     fi
-    sleep 1
 done
 
 echo -e "\e[1;31m 2. collect static \e[0m"
