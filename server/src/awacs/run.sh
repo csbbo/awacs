f32e543ca645857b8e2fee5bf177d8aca67f392a
shaobo.chen <shaobo.chen@chaitin.com>
2020-11-24 10:13:25 +0800

    fix: collectstatic not input

--- server/src/awacs/run.sh
+++ server/src/awacs/run.sh
@@ -10,7 +10,7 @@ while :; do
 done
 
 echo -e "\e[1;31m 2. collect static \e[0m"
-python3 manage.py collectstatic
+python3 manage.py collectstatic --no-input
 
 echo -e "\e[1;31m 3. running \e[0m"
 daphne -b 0.0.0.0 -p 8002 awacs.asgi:application
